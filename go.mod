98f199aad727f23789ce1d1f9e343e41b4ac5640
Denis Tingajkin <denis.tingajkin@xored.com>
2020-07-16 19:23:10 +0700

    implement registry proxy dns application
    Signed-off-by: Denis Tingajkin <denis.tingajkin@xored.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/networkservicemesh/cmd-registry-proxy-dns
 
 go 1.14
+
+require (
+	github.com/antonfisher/nested-logrus-formatter v1.1.0
+	github.com/edwarnicke/exechelper v1.0.1
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/networkservicemesh/api v0.0.0-20200626035416-b2c37e63dc22
+	github.com/networkservicemesh/sdk v0.0.0-20200715140638-09de238589f2
+	github.com/sirupsen/logrus v1.6.0
+	github.com/spiffe/go-spiffe/v2 v2.0.0-alpha.4.0.20200528145730-dc11d0c74e85
+	github.com/stretchr/testify v1.5.1
+	google.golang.org/grpc v1.30.0
+)
--- main.go
+++ main.go
@@ -16,6 +16,119 @@
 
 package main
 
-func main() {
+import (
+	"context"
+	"net/url"
+	"os"
+	"time"
 
+	"github.com/networkservicemesh/sdk/pkg/registry/common/connect"
+	"github.com/networkservicemesh/sdk/pkg/registry/common/dnsresolve"
+	"github.com/networkservicemesh/sdk/pkg/registry/common/swap"
+
+	nested "github.com/antonfisher/nested-logrus-formatter"
+	"github.com/kelseyhightower/envconfig"
+	api_registry "github.com/networkservicemesh/api/pkg/api/registry"
+	"github.com/sirupsen/logrus"
+	"github.com/spiffe/go-spiffe/v2/spiffetls/tlsconfig"
+	"github.com/spiffe/go-spiffe/v2/workloadapi"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	"github.com/networkservicemesh/sdk/pkg/networkservice/chains/registry"
+
+	"github.com/networkservicemesh/sdk/pkg/registry/core/chain"
+	"github.com/networkservicemesh/sdk/pkg/tools/debug"
+	"github.com/networkservicemesh/sdk/pkg/tools/grpcutils"
+	"github.com/networkservicemesh/sdk/pkg/tools/log"
+	"github.com/networkservicemesh/sdk/pkg/tools/signalctx"
+)
+
+// Config is configuration for cmd-registry-proxy-dns
+type Config struct {
+	ListenOn       url.URL `default:"unix:///listen.on.socket" desc:"url to listen on" split_words:"true"`
+	ProxyNSMgrURL  url.URL `desc:"url to proxy nsmgr"`
+	PublicNSMgrURL url.URL `desc:"url to nsmgr"`
+	Domain         string  `desc:"domain name"`
+}
+
+func main() {
+	// Setup context to catch signals
+	ctx := signalctx.WithSignals(context.Background())
+	ctx, cancel := context.WithCancel(ctx)
+
+	// Setup logging
+	logrus.SetFormatter(&nested.Formatter{})
+	logrus.SetLevel(logrus.TraceLevel)
+	ctx = log.WithField(ctx, "cmd", os.Args[0])
+
+	// Debug self if necessary
+	if err := debug.Self(); err != nil {
+		log.Entry(ctx).Infof("%s", err)
+	}
+
+	startTime := time.Now()
+
+	// Get config from environment
+	config := &Config{}
+	if err := envconfig.Usage("registry-proxy-dns", config); err != nil {
+		logrus.Fatal(err)
+	}
+	if err := envconfig.Process("registry-proxy-dns", config); err != nil {
+		logrus.Fatalf("error processing config from env: %+v", err)
+	}
+
+	log.Entry(ctx).Infof("Config: %#v", config)
+
+	// Get a X509Source
+	source, err := workloadapi.NewX509Source(ctx)
+	if err != nil {
+		logrus.Fatalf("error getting x509 source: %+v", err)
+	}
+	svid, err := source.GetX509SVID()
+	if err != nil {
+		logrus.Fatalf("error getting x509 svid: %+v", err)
+	}
+	logrus.Infof("SVID: %q", svid.ID)
+
+	nseChain := chain.NewNetworkServiceEndpointRegistryServer(
+		dnsresolve.NewNetworkServiceEndpointRegistryServer(),
+		swap.NewNetworkServiceEndpointRegistryServer(config.Domain, &config.ProxyNSMgrURL, &config.PublicNSMgrURL),
+		connect.NewNetworkServiceEndpointRegistryServer(func(ctx context.Context, cc grpc.ClientConnInterface) api_registry.NetworkServiceEndpointRegistryClient {
+			return chain.NewNetworkServiceEndpointRegistryClient(api_registry.NewNetworkServiceEndpointRegistryClient(cc))
+		}),
+	)
+
+	nsChain := chain.NewNetworkServiceRegistryServer(
+		dnsresolve.NewNetworkServiceRegistryServer(),
+		swap.NewNetworkServiceRegistryServer(config.Domain),
+		connect.NewNetworkServiceRegistryServer(func(ctx context.Context, cc grpc.ClientConnInterface) api_registry.NetworkServiceRegistryClient {
+			return chain.NewNetworkServiceRegistryClient(api_registry.NewNetworkServiceRegistryClient(cc))
+		}),
+	)
+
+	// Create GRPC Server and register services
+	server := grpc.NewServer(grpc.Creds(credentials.NewTLS(tlsconfig.MTLSServerConfig(source, source, tlsconfig.AuthorizeAny()))))
+
+	registry.NewServer(nsChain, nseChain).Register(server)
+
+	srvErrCh := grpcutils.ListenAndServe(ctx, &config.ListenOn, server)
+	exitOnErr(ctx, cancel, srvErrCh)
+	log.Entry(ctx).Infof("Startup completed in %v", time.Since(startTime))
+	<-ctx.Done()
+}
+
+func exitOnErr(ctx context.Context, cancel context.CancelFunc, errCh <-chan error) {
+	// If we already have an error, log it and exit
+	select {
+	case err := <-errCh:
+		log.Entry(ctx).Fatal(err)
+	default:
+	}
+	// Otherwise wait for an error in the background to log and cancel
+	go func(ctx context.Context, errCh <-chan error) {
+		err := <-errCh
+		log.Entry(ctx).Error(err)
+		cancel()
+	}(ctx, errCh)
 }
