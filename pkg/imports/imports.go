aeee31108d2d8a79704a2789fab7e547a7884eb3
Mikhail <avramenkomihail15@gmail.com>
2021-04-26 15:56:22 +0700

    removed unused import
    Signed-off-by: Mikhail <avramenkomihail15@gmail.com>

--- pkg/imports/imports.go
+++ pkg/imports/imports.go
@@ -14,7 +14,6 @@ import (
 	_ "github.com/networkservicemesh/sdk/pkg/tools/log"
 	_ "github.com/networkservicemesh/sdk/pkg/tools/log/logruslogger"
 	_ "github.com/networkservicemesh/sdk/pkg/tools/opentracing"
-	_ "github.com/networkservicemesh/sdk/pkg/tools/signalctx"
 	_ "github.com/networkservicemesh/sdk/pkg/tools/spire"
 	_ "github.com/sirupsen/logrus"
 	_ "github.com/spiffe/go-spiffe/v2/bundle/x509bundle"
